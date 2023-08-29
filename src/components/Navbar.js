4aaea20d0eea95465a1357f3cdddf266f557ad85
Dungha125 <dungha122405@gmail.com>
2023-08-29 14:38:53 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <ul className="menu">
+            <li>TRANG CHỦ</li>
+            <li>PHIM LẺ</li>
+            <li>PHIM BỘ</li>
+            <li>TOP PHIM</li>
+    </ul>
+  )
+}
+
+export default Navbar
---
+++ src/components/Slidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function slidebar() {
+  return (
+    <div className='slide'></div>
+  )
+}
+
+export default slidebar
